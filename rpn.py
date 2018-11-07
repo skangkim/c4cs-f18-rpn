4aacbb13b2b699f6f311c8aeb7ddb357069e2df9
Sukang Sabina Kim <sukang@umich.edu>
2018-11-06 21:33:13 -0500

    modified

--- rpn.py
+++ rpn.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
+import operator
+
+op = {
+		'+': operator.add,
+		'-': operator.sub,
+		'*': operator.mul,
+		'/': operator.floordiv
+        }
 
 def calculate(arg):
-    pass
+	stack = arg.split()
+	while len(stack) > 1:
+		token = stack.pop()
+		try:
+			value = int(token)
+			stack.append(value)
+		except ValueError:
+			val2 = int(stack.pop())
+			val1 = int(stack.pop())
+			func = op[token]
+			result = func(val1, val2)
+			stack.append(result)
+	return int(stack[0])
 
 def main():
-    while True:
-        calculate(input("rpn calc> "))
-
-if __name__ == '__main__':
-    main()
+	while True:
+		result = calculate(input("rpn calc> "))
+		print(result)
+        
+if __name__=='__main__':
+	main()
